ba08739d8a608bb4da08a3e77b08b11087af2a0c
rbonazzola <rodbonazzola@gmail.com>
2022-06-22 11:59:27 +0100

    Added config files

---
+++ config/cli_args.py
@@ -0,0 +1,249 @@
+import argparse
+from config.load_config import load_yaml_config, to_dict, flatten_dict, rsetattr, rgetattr
+from copy import deepcopy
+
+class ArgumentAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        rsetattr(namespace, self.dest, values)
+
+class kwargs_append_action(argparse.Action):
+    """
+    argparse action to split an argument into KEY=VALUE form
+    on append to a dictionary.
+    """
+
+    def __call__(self, parser, args, values, option_string=None):
+        try:
+            d = dict(map(lambda x: x.split('='),values))
+        except ValueError as ex:
+            raise argparse.ArgumentError(self, f"Could not parse argument \"{values}\" as k1=v1 k2=v2 ... format")
+        setattr(args, self.dest, d)
+
+network_architecture_args = {
+    ("--n_channels",): {
+        "help": "Number of channels (feature maps). If the rest of the --n_channels_* arguments are not provided, it will assign these numbers to the encoder, content decoder and style decoder.",
+        "nargs": "+", "type": int,
+        "action": ArgumentAction,
+        "dest": "config.network_architecture.convolution.channels"
+    },
+    ("--reduction_factors",): {
+        "help": "Decimation factors for the mesh",
+        "nargs": "+", "type": int,
+        "action": ArgumentAction,
+        "dest": "config.network_architecture.pooling.downsampling_factors"},
+    ("--n_channels_enc",): {
+        "help": "Number of channels (feature maps) in the encoder, from input to the most hidden layer.",
+        "nargs": "+", "type": int,
+        "action": ArgumentAction,
+        "dest": "config.network_architecture.convolution.channels_enc"},
+    ("--n_channels_dec_c",): {
+        "help": "Number of channels (feature maps) in the content decoder, from the most hidden layer to the output.",
+        "nargs": "+", "type": int,
+        "action": ArgumentAction,
+        "dest": "config.network_architecture.convolution.channels_dec_c"},
+    ("--n_channels_dec_s",): {
+        "help": "Number of channels (feature maps) in the style decoder, from the most hidden layer to the output.",
+        "nargs": "+", "type": int,
+        "action": ArgumentAction,
+        "dest": "config.network_architecture.convolution.channels_dec_s"},
+    ("--latent_dim_c",): {
+        "help": "Dimension of the content part of the latent space",
+        "type": int,
+        "action": ArgumentAction,
+        "dest": "config.network_architecture.latent_dim_c"},
+    ("--latent_dim_s",): {
+        "help": "Dimension of the style part of the latent space",
+        "type": int,
+        "action": ArgumentAction,
+        "dest": "config.network_architecture.latent_dim_s"},
+    ("--activation_function",): {
+        "help": "Activation functions to be used",
+        "nargs": "+", "type": str,
+        "action": ArgumentAction,
+        "dest": "config.network_architecture.activation_function"},
+    ("--polynomial_degree",): {
+        "help": "Chebyshev polynomial degree",
+        "nargs": "+", "type": int,
+        "action": ArgumentAction,
+        "dest": "config.network_architecture.convolution.parameters.polynomial_degree"},
+    ("--z_aggr_function",): {
+        "help": "Temporal aggregation method",
+        "type": str,
+        "action": ArgumentAction,
+        "dest": "config.network_architecture.z_aggr_function"},
+    ("--z_aggr_function",): {
+        "help": "Temporal aggregation method",
+        "type": str,
+        "action": ArgumentAction,
+        "dest": "config.network_architecture.z_aggr_function"},
+    ("--only_decoder",): {
+        "help": "Flag to run only the decoder",
+        "action": "store_true"},
+    ("--only_encoder",): {
+        "help": "Flag to run only the encoder",
+        "action": "store_true"},
+
+    #("--phase_input" ): {
+    #    "help": "If this flag is set, the phase embedding is not applied to the input mesh coordinates.",
+    #    "default": True,
+    #    "dest": "config.network_architecture.phase_input",
+    #    "action": argparse.BooleanOptionalAction}
+}
+
+loss_args = {
+    ("--reconstruction_loss_type",): {
+        "help": "Type of reconstruction loss",
+        "dest": "config.loss.reconstruction_c.type",
+        "type": str,
+        "action": ArgumentAction},
+    ("--w_kl",): {
+        "help": "weight of KL term",
+        "dest": "config.loss.regularization.weight",
+        "type": float,
+        "action": ArgumentAction},
+    ("--w_s",): {
+        "help": "weight of the \"style\" reconstruction term in the lost function",
+        "dest": "config.loss.reconstruction_s.weight",
+        "type": float,
+        "action": ArgumentAction},
+}
+
+dataset_args = {
+    ("--dataset.amplitude_static_max",): {
+        "help": "" ,
+        "dest": "config.dataset.parameters.amplitude_static_max" , 
+        "type": float,
+        "action": ArgumentAction},
+    ("--dataset.amplitude_dynamic_max",): {
+        "help": "",
+        "dest": "config.dataset.parameters.amplitude_dynamic_max" , 
+        "type": float,
+        "action": ArgumentAction},
+    ("--dataset.N_subjects",): {
+        "help": "",
+        "dest": "config.dataset.parameters.N" , 
+        "type": int,
+        "action": ArgumentAction},
+    ("--dataset.N_timeframes",): {
+        "help": "",
+        "dest": "config.dataset.parameters.T" , 
+        "type": int,
+        "action": ArgumentAction},
+    ("--dataset.freq_max",): {
+        "help": "",
+        "dest": "config.dataset.parameters.freq_max" , 
+        "type": int,
+        "action": ArgumentAction},
+    ("--dataset.l_max",): {
+        "help": "",
+        "dest": "config.dataset.parameters.l_max" , 
+        "type": int,
+        "action": ArgumentAction},
+    ("--dataset.mesh_resolution",): {
+        "help": "",
+        "dest": "config.dataset.parameters.mesh_resolution" , 
+        "type": int,
+        "action": ArgumentAction},
+    ("--dataset.center_around_mean",): {
+        "help": "Not working! Always sets this value to True.",
+        "dest": "config.dataset.preprocessing.center_around_mean" , 
+        "type": bool,
+        "action": ArgumentAction}
+}
+
+training_args = {
+    ("--learning_rate", "-lr",): {
+        "help": "Learning rate",
+        "dest": "config.optimizer.parameters.lr",
+        "type": float,
+        "action": ArgumentAction},
+    ("--batch_size",): {
+        "help": "Training batch size. If provided will overwrite the batch size from the configuration file.",
+        "dest": "config.batch_size",
+        "type": int,
+        "action": ArgumentAction},
+    ("--partition_lengths", "--partition-lengths"): {
+        "nargs":"+", "help": "List of two [or three] integers (floats) representing the number of samples (fraction of samples) to be used for training, validation [and testing].",
+        "dest": "config.sample_sizes",
+        "action": ArgumentAction},
+}
+
+mlflow_args = {
+    ("--disable_mlflow_logging",): {
+        "help": "Set this flag if you don't want to log the run's data to MLflow.",
+        "default": False,
+        "action": "store_true"},
+    ("--mlflow_experiment",): {
+        "help": "MLflow experiment's name",
+        "dest": "config.mlflow.experiment_name",
+        "action": ArgumentAction
+    },
+    ("--additional_mlflow_params",): {
+        "nargs": '+',
+        "required": False,
+        "dest": "config.additional_mlflow_params",
+        "action": kwargs_append_action,
+        "metavar": "KEY=VALUE",
+        "help": "Add additional key/value params to MLflow."
+    },
+    ("--additional_mlflow_tags",): {
+        "nargs": '+',
+        "required": False,
+        "dest": "config.additional_mlflow_tags",
+        "action": kwargs_append_action,
+        "metavar": "KEY=VALUE",
+        "help": "Add additional key/value tags to MLflow."
+    }
+}
+
+#   ("--mlflow_config",): {
+#       "action": LoadYamlConfig,
+#       "help": "YAML configuration file containing information to log model information to MLflow.",
+#       "dest": "config.mlflow"},
+
+#########################################################################
+#### Put all the arguments together
+#########################################################################
+
+CLI_args = {
+    ("-c", "--conf",): {
+        "help": "Path of a YAML configuration file to be used as a reference configuration.",
+        "default": "config_files/config_folded_c_and_s.yaml",
+        "dest": "yaml_config_file"
+    },
+    ** network_architecture_args,
+    ** loss_args,
+    ** training_args,
+    ** dataset_args,
+    ** mlflow_args,
+    ("--show_config",): {
+        "default": False,
+        "action": "store_true",
+        "help": "Display run's configuration"
+    },
+    ("--dry-run", "--dry_run"): {
+        "dest": "dry_run",
+        "default": False,
+        "action": "store_true",
+        "help": "Dry run: just prints out the parameters of the execution but performs no training.",
+    },
+    ("--log_computational_graph",): {
+        "default": False,
+        "action": "store_true",
+        "help": "If True, will log the computational graph as an artifact (not fully functional due to limitations of the torchviz library)"
+    }
+}
+
+
+def overwrite_config_items(ref_config, config_to_replace):
+    '''
+    params:
+    :: ref_config ::
+    :: config_to_replace ::
+    '''
+
+    config = deepcopy(ref_config)
+    for k, v in flatten_dict(to_dict(config_to_replace)).items():
+        rsetattr(config, k, v)
+
+    return config
---
+++ config/load_config.py
@@ -0,0 +1,161 @@
+import os
+import yaml
+import functools
+from argparse import Namespace
+from collections.abc import MutableMapping
+
+CONFIG_FILES_DIR = "config_files"
+
+def rsetattr(obj, attr, val):
+    pre, _, post = attr.rpartition('.')
+    return setattr(rgetattr(obj, pre) if pre else obj, post, val)
+
+
+# From: https://stackoverflow.com/questions/31174295/getattr-and-setattr-on-nested-objects/31174427?noredirect=1#comment86638618_31174427
+def rgetattr(obj, attr, *args):
+    def _getattr(obj, attr):
+        try:
+            return getattr(obj, attr, *args)
+        except AttributeError:
+            setattr(obj, attr, Namespace())
+            return getattr(obj, attr, *args)
+    return functools.reduce(_getattr, [obj] + attr.split('.'))
+
+
+def is_yaml_file(x):
+    if isinstance(x, str):
+        return x.endswith("yaml") or x.endswith("yml")
+    return False
+
+
+def get_repo_rootdir():
+    import shlex
+    from subprocess import check_output
+    repo_rootdir = check_output(shlex.split("git rev-parse --show-toplevel")).strip().decode('ascii')
+    return repo_rootdir
+
+
+def unfold_config(token, no_unfolding_for=[]):
+    '''
+    Parameters: 
+      token: a recursive structure composed of either 1. a path to a yaml file or 2. a dictionary composed of such structures.
+      no_unfolding_for: a list of dict keys for which the yaml shouldn't be unfolded, and instead kept as a path
+    Returns: A dictionary with all the yaml files replaces by their content.
+    '''
+
+    #
+    if is_yaml_file(token):
+        #TODO: COMMENT AND DOCUMENT THIS!!!
+        yaml_file_base = token
+        try:            
+            yaml_dir = get_repo_rootdir()
+            yaml_file = os.path.join(yaml_dir, yaml_file_base)
+            token = yaml.safe_load(open(yaml_file))
+        except FileNotFoundError:
+            yaml_dir = os.path.join(get_repo_rootdir(), CONFIG_FILES_DIR)
+            yaml_file = os.path.join(yaml_dir, yaml_file_base)
+            token = yaml.safe_load(open(yaml_file))
+
+    if isinstance(token, dict):
+        for k, v in token.items():
+            if k not in no_unfolding_for:
+                token[k] = unfold_config(v, no_unfolding_for)
+
+    return token
+
+
+def to_dict(token):
+    '''
+    Converts a (possibly nested) namespace to a nested dictionary
+    '''
+
+    if isinstance(token, Namespace):
+        namespace_as_dict = token.__dict__
+        token = {k: to_dict(v) for k, v in namespace_as_dict.items()}
+    return token
+
+
+def recursive_namespace(dd):
+    '''
+    Converts a (possibly nested) dictionary into a namespace.
+    This allows for auto-completion.
+    '''
+    for d in dd:
+        has_any_dicts = False
+        if isinstance(dd[d], dict):
+            dd[d] = recursive_namespace(dd[d])
+            has_any_dicts = True
+    return Namespace(**dd)
+
+
+def sanity_check(config):
+    
+    '''
+    Perform sanity check on the provided configuration.
+    '''
+
+    pol_deg_dim = len(config.network_architecture.convolution.parameters.polynomial_degree)
+    downsampling_factors_dim = len(config.network_architecture.pooling.parameters.downsampling_factors)
+    
+    try:
+      n_channels_dim = len(config.network_architecture.convolution.channels)
+    except:
+      n_channels_dim = len(config.network_architecture.convolution.channels_enc)
+
+    if not ((pol_deg_dim == downsampling_factors_dim) and (pol_deg_dim == n_channels_dim)):       
+       raise ValueError(
+          f"Dimensions of polynomial degrees, downsampling factors and number of channels should match \
+          (but are {pol_deg_dim}, {downsampling_factors_dim} and {n_channels_dim}.)"
+       )
+
+
+def _flatten_dict_gen(d, parent_key, sep):
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, MutableMapping):
+            yield from flatten_dict(v, new_key, sep=sep).items()
+        else:
+            yield new_key, v
+
+
+def flatten_dict(d: MutableMapping, parent_key: str = '', sep: str = '.'):
+    return dict(_flatten_dict_gen(d, parent_key, sep))
+
+
+def load_yaml_config(yaml_config_file):
+
+    # config = yaml.safe_load(config)    
+    config = unfold_config(yaml_config_file)    
+        # I am using a namespace instead of a dictionary mainly because it enables auto-completion
+    config = recursive_namespace(config)
+
+    # The following parameters are meant to be lists of numbers, so they are parsed here from their string representation in the YAML file.
+    config.network_architecture.convolution.parameters.polynomial_degree = \
+    [int(x) for x in config.network_architecture.convolution.parameters.polynomial_degree.split()]
+    
+    config.network_architecture.pooling.parameters.downsampling_factors = \
+    [int(x) for x in config.network_architecture.pooling.parameters.downsampling_factors.split()]
+
+    if hasattr(config.network_architecture.convolution, "channels"):
+      config.network_architecture.convolution.channels = \
+      [int(x) for x in config.network_architecture.convolution.channels.split()]
+
+    if hasattr(config.network_architecture.convolution, "channels_enc"):
+      config.network_architecture.convolution.channels_enc = \
+      [int(x) for x in config.network_architecture.convolution.channels_enc.split()]
+  
+    if hasattr(config.network_architecture.convolution, "channels_dec_c"):
+      config.network_architecture.convolution.channels_dec_c = \
+      [int(x) for x in config.network_architecture.convolution.channels_dec_c.split()]
+
+    if hasattr(config.network_architecture.convolution, "channels_dec_s"):
+      config.network_architecture.convolution.channels_dec_s = \
+      [int(x) for x in config.network_architecture.convolution.channels_dec_s.split()]
+
+    if hasattr(config.network_architecture, "activation_function"):
+      config.network_architecture.activation_function = \
+      [x for x in config.network_architecture.activation_function.split()]
+    
+    sanity_check(config)
+
+    return config
