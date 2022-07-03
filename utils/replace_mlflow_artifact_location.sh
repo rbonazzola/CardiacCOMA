2850f8a78e88ad72cf69e1c409bbf6c8bf8e5ec0
rbonazzola <scrb@leeds.ac.uk>
2022-07-03 10:03:56 +0100

    Updated MLflow artifact URI replacement script

--- utils/replace_mlflow_artifact_location.sh
+++ utils/replace_mlflow_artifact_location.sh
@@ -1,12 +1,12 @@
-ORIGINAL_PATH="\/usr\/not-backed-up\/scrb\/src"
-#ORIGINAL_PATH="\/app"
+OPATHS=("\/usr\/not-backed-up\/scrb\/src" "\/app")
 NEW_PATH="\/home\/rodrigo\/CISTIB\/repos"
 MLFLOW_URI="../mlruns"
 
-
+for ORIGINAL_PATH in ${OPATHS[@]}; do
 for EXPERIMENT in `ls ${MLFLOW_URI}`; do 
   for RUN in `ls ${MLFLOW_URI}/$EXPERIMENT/`; do 
     sed -i 's/'${ORIGINAL_PATH}'/'${NEW_PATH}'/' ${MLFLOW_URI}/${EXPERIMENT}/${RUN}/meta.yaml | grep artifact 
     echo $EXPERIMENT/$RUN
   done
 done  
+done
