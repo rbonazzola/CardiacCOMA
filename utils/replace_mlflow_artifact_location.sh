65e071667632c0e5ac7db99186760c22a46c05d3
rbonazzola <scrb@leeds.ac.uk>
2022-07-08 14:24:11 +0100

    Updated script to fix MLflow runs

--- utils/replace_mlflow_artifact_location.sh
+++ utils/replace_mlflow_artifact_location.sh
@@ -1,11 +1,14 @@
 OPATHS=("\/usr\/not-backed-up\/scrb\/src" "\/app")
 NEW_PATH="\/home\/rodrigo\/CISTIB\/repos"
 MLFLOW_URI="../mlruns"
+EXPERIMENT_PATTERN="\/[0-9]\/"
 
 for ORIGINAL_PATH in ${OPATHS[@]}; do
 for EXPERIMENT in `ls ${MLFLOW_URI}`; do 
   for RUN in `ls ${MLFLOW_URI}/$EXPERIMENT/`; do 
     sed -i 's/'${ORIGINAL_PATH}'/'${NEW_PATH}'/' ${MLFLOW_URI}/${EXPERIMENT}/${RUN}/meta.yaml | grep artifact 
+    sed -i "s/experiment_id: '.'/experiment_id: '"${EXPERIMENT}"'/g" ${MLFLOW_URI}/${EXPERIMENT}/${RUN}/meta.yaml | grep experiment 
+    sed -i "s/"${EXPERIMENT_PATTERN}"/\/"${EXPERIMENT}"\//g" ${MLFLOW_URI}/${EXPERIMENT}/${RUN}/meta.yaml | grep experiment 
     echo $EXPERIMENT/$RUN
   done
 done  
